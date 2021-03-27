203c8ea5688f9505d9fc1b9da73f339657bc60fb
Peter2221 <pmbasen@gmail.com>
2021-03-27 16:37:13 +0100

    feat: add showcase image

--- src/domains/Landing/LandingPage/LandingPage.tsx
+++ src/domains/Landing/LandingPage/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Button, Container, Grid, Typography } from '@material-ui/core';
 import { MainSubtitle, MainTitle } from './LandingPage.styled';
+import { ShowcaseImage } from '@shared/svgs/ShowcaseImage';
 
 interface IProps {}
 
@@ -8,19 +9,29 @@ const LandingPage: React.FC<IProps> = () => {
     return (
         <>
             <Container>
-                <Grid container>
+                <Grid container direction="row" justify="center">
                     <Grid item xs={6}>
                         <MainTitle>Professional IT Products</MainTitle>
                         <MainSubtitle>
                             Do you need app, website or design? Hire the best
                             developers and let them make it for you.
                         </MainSubtitle>
-                        <Button variant="contained" color="primary">
-                            Login
-                        </Button>
-                        <Button variant="contained">Register</Button>
+                        <Box display="flex" flexDirection="row" mt={4}>
+                            <Box mr={3}>
+                                <Button variant="contained" color="primary">
+                                    Login
+                                </Button>
+                            </Box>
+                            <Box>
+                                <Button variant="contained" color="primary">
+                                    Register
+                                </Button>
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid item xs={6}></Grid>
+                    <Grid item xs={6}>
+                        <ShowcaseImage width={600} height={400} />
+                    </Grid>
                 </Grid>
             </Container>
         </>
