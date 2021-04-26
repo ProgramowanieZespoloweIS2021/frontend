8c7d442756e3deccc9a629159de0ec826103253b
Peter2221 <pmbasen@gmail.com>
2021-04-26 20:59:00 +0200

    Add offers dashboard chips, addBtn

--- src/domains/OffersDashboard/OffersDashboardPage.styled.ts
+++ src/domains/OffersDashboard/OffersDashboardPage.styled.ts
@@ -1,1 +1,17 @@
+import styled, { css } from 'styled-components';
+import { Chip, Fab } from '@material-ui/core';
 export {};
+
+export const TagChip = styled(Chip)(
+    ({ theme }) => css`
+        margin-right: 5px;
+    `,
+);
+
+export const AddBtnContainer = styled.div(
+    ({ theme }) => css`
+        position: fixed;
+        bottom: 10%;
+        right: 10%;
+    `,
+);
--- src/domains/OffersDashboard/OffersDashboardPage.tsx
+++ src/domains/OffersDashboard/OffersDashboardPage.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { DefaultText, Header } from '@components/_universal/Typography.styled';
+import { mockOffers } from '@domains/Landing/OffersList/mockOffers';
 import {
     Container,
     IconButton,
@@ -6,9 +9,12 @@ import {
     ListItemText,
     Divider,
     List,
+    Avatar,
+    ListItemAvatar,
+    Fab,
 } from '@material-ui/core';
-import { Delete, Edit } from '@material-ui/icons';
-import React from 'react';
+import { Delete, Edit, Add } from '@material-ui/icons';
+import { AddBtnContainer, TagChip } from './OffersDashboardPage.styled';
 
 interface IProps {}
 
@@ -16,22 +22,43 @@ const OffersDashboardPage: React.FC<IProps> = () => {
     return (
         <>
             <Container>
-                <h1>Offers Dashboard</h1>
+                <Header>Offers Dashboard</Header>
                 <List>
-                    <ListItem>
-                        <ListItemText>Some text</ListItemText>
-                        <ListItemSecondaryAction>
-                            <IconButton edge="end" aria-label="edit">
-                                <Edit />
-                            </IconButton>
-                            <IconButton edge="end" aria-label="delete">
-                                <Delete />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                    <Divider />
+                    {mockOffers.map(({ title, thumbnails, tags }) => (
+                        <>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar src={thumbnails[0].url} />
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <DefaultText>{title}</DefaultText>
+                                    {tags.map((tag) => (
+                                        <TagChip
+                                            variant="outlined"
+                                            size="small"
+                                            label={tag.name}
+                                        />
+                                    ))}
+                                </ListItemText>
+                                <ListItemSecondaryAction>
+                                    <IconButton edge="end" aria-label="edit">
+                                        <Edit />
+                                    </IconButton>
+                                    <IconButton edge="end" aria-label="delete">
+                                        <Delete />
+                                    </IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                            <Divider />
+                        </>
+                    ))}
                 </List>
             </Container>
+            <AddBtnContainer>
+                <Fab color="primary" aria-label="add">
+                    <Add />
+                </Fab>
+            </AddBtnContainer>
         </>
     );
 };
