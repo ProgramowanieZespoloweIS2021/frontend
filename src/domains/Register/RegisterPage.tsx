521e6c82b45d66fbf98101a45782767189abd62d
Wadowski <domwadowski@gmail.com>
2021-05-23 11:48:12 +0200

    Center login and register

--- src/domains/Register/RegisterPage.tsx
+++ src/domains/Register/RegisterPage.tsx
@@ -8,6 +8,7 @@ import {
     InfoText,
     LoginWrapper,
     Button,
+    Wrapper,
 } from './RegisterPage.styled';
 import Form from './_components/Form';
 import paths from '@shared/paths';
@@ -23,7 +24,7 @@ const RegisterPage: React.FC<IProps> = () => {
 
     return (
         <>
-            <Grid container justify="center" alignItems="center" spacing={4}>
+            <Wrapper container justify="center" alignItems="center" spacing={4}>
                 <Grid item sm={6} md={3} xs={12}>
                     <Container elevation={2}>
                         <RegisterHeader>Register</RegisterHeader>
@@ -41,7 +42,7 @@ const RegisterPage: React.FC<IProps> = () => {
                         </LoginWrapper>
                     </Container>
                 </Grid>
-            </Grid>
+            </Wrapper>
         </>
     );
 };
