ab992ab09325d7935d920a8c454883da85c8cadf
Wadowski <domwadowski@gmail.com>
2021-06-12 21:20:44 +0200

    style fixes

--- src/domains/Account/AccountPage.tsx
+++ src/domains/Account/AccountPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
-
-import { LoginHeader, Wrapper } from './AccountPage.styled';
+import { LoginHeader, Wrapper, Item } from './AccountPage.styled';
 import ChangePasswordForm from './_components/ChangePasswordForm';
 import ChangeUserDetailsForm from './_components/ChangeUserDetailsForm';
 import { SectionContainer } from '@components/_form-elements/SectionContainer';
@@ -18,19 +16,19 @@ const USER = {
 const AccountPage: React.FC<IProps> = () => {
     return (
         <>
-            <Wrapper container justify="center" spacing={4}>
-                <Grid item xl={6}>
+            <Wrapper>
+                <Item>
                     <SectionContainer>
                         <LoginHeader>Edit account</LoginHeader>
                         <ChangeUserDetailsForm {...USER} />
                     </SectionContainer>
-                </Grid>
-                <Grid item xl={6}>
+                </Item>
+                <Item>
                     <SectionContainer>
                         <LoginHeader>Change password</LoginHeader>
                         <ChangePasswordForm />
                     </SectionContainer>
-                </Grid>
+                </Item>
             </Wrapper>
         </>
     );
