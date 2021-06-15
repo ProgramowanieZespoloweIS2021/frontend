9d0be41f5000372a954329b747dec04be78a003f
Wadowski <domwadowski@gmail.com>
2021-06-15 19:50:01 +0200

    Fix auth

--- src/routes/AuthContainer.tsx
+++ src/routes/AuthContainer.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectUserDetails } from '@state/_redux/user/selectors';
+import { isAuthorized } from '@state/_redux/user/selectors';
 import { getUser } from '@state/_redux/user/actions';
 
 interface IProps {}
 
 export const AuthContainer: React.FC<IProps> = ({ children }) => {
-    const { id } = useSelector(selectUserDetails);
+    const isAuth = useSelector(isAuthorized);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getUser.request(''));
     }, []);
 
-    return <>{id && children}</>;
+    return <>{isAuth !== null && children}</>;
 };
